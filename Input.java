1eb9047618d2bbadbe28dd7abb36acb9055247ec
mohitsing115 <mohitsharma9285@gmail.com>
2026-06-01 14:20:53 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("\nName: " + name);
+        System.out.println("Age: " + age);
+
+        sc.close();
+
+        
+    }
+}
